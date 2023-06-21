be2acac22c742b33b370da1167c1636b62eba981
sacred <sacred@sacred.com>
2023-06-21 19:16:40 +0300

    removed old file

--- index.js
+++ index.js
@@ -48,37 +48,3 @@ app.use("/user", router);
 app.listen(8989, () => {
   console.log("server listen on port: 8989");
 });
-
-//Алгоритм луна, проверка карты
-
-const checkCreditCard = (card) => {
-  const check = card
-    .split("")
-    .filter((item) => item !== " ")
-    .map((item) => parseInt(item))
-    .filter((item) => item);
-
-  if (check.length !== 16) {
-    return false;
-  }
-
-  const result = check.map((item, index) => {
-    let current = item;
-
-    if (index % 2 === 0) {
-      current = current * 2;
-
-      if (current > 9) {
-        current = String(current)
-          .split("")
-          .reduce((prev, item) => prev + parseInt(item), 0);
-      }
-    }
-    return current;
-  });
-
-  return result.reduce((prev, item) => prev + item, 0) % 10 === 0;
-};
-
-const test = "4242 4242 4242 4242";
-console.log(checkCreditCard(test));
